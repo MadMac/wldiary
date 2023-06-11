383d1a69b141ba4e8138021a3b2233193c2964b8
Markus Hällfors <markus@hallfors.com>
2023-06-11 20:14:47 +0300

    Add basic SQLite connection and migration to the backend

---
+++ src-tauri/migrations/2023-06-11-154910_create_log/up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE daily_logs (
+  	id TEXT PRIMARY KEY,
+  	content TEXT NOT NULL,
+	log_date TEXT NOT NULL
+)
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use diesel::{Connection, SqliteConnection};
+use diesel_migrations::EmbeddedMigrations;
+use diesel_migrations::{embed_migrations, MigrationHarness};
+use log::{debug, info};
+
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +15,13 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("debug"));
+
+    info!("Running migrations.");
+    let mut conn = SqliteConnection::establish("log_files.db").unwrap();
+    conn.run_pending_migrations(MIGRATIONS).unwrap();
+
+    info!("Starting Tauri backend.");
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
