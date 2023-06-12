0bba0bbc6446205a074ed6cbd1752db4c779d4a9
Markus Hällfors <markus@hallfors.com>
2023-06-12 20:47:46 +0300

    Add a basic layout and texteditor

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,39 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet  from "./components/Greet.vue";
+import TextEditor from "./components/TextEditor.vue";
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+  <div id="container">
+    <div id="sidebar">
+      test
     </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
+    <div id="editor">
+      <TextEditor />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
+#container {
+  display: flex;
+  flex-direction: row;
+  margin: 0px;
+  height: 100%;
+  overflow: hidden;
 }
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+#sidebar {
+  border-right: 1px solid black;
+  height: 100%;
+  min-width: 200px;
+  flex: 1;
+}
+
+#editor {
+  height: 100%;
+  flex: 5;
+  min-width: 500px;
 }
 </style>
---
+++ src/components/TextEditor.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { debounce } from 'lodash-es'
+
+const content = ref("");
+
+const update = debounce((e) => {
+	content.value = e.target.value
+}, 100)
+
+</script>
+
+<template>
+	<div class="editor">
+		<textarea class="input" :value="content" @input="update"></textarea>
+	</div>
+</template>
+
+<style scoped>
+.editor {
+	height: 100%;
+	display: flex;
+}
+
+.input:focus {
+	outline: 0px;
+	box-shadow: 0px;
+	outline-offset: 0px;
+}
+
+.input {
+	resize: none;
+	border: 0px;
+	height: 100%;
+	padding-top: 30px;
+	padding-bottom: 30px;
+	padding-left: 50px;
+	padding-right: 50px;
+	background-color: #2d2d2d;
+	color: #fff;
+	flex: 1;
+	font-size: 1.1em;
+	/* Currently makes the caret too tall */
+	/* line-height: 2em; */
+}
+</style>
